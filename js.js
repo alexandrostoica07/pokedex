9ee6069ab9af7a1b8be3c4748dff3203aab7fcbb
alexandrostoica07 <alexandrostoica07@gmail.com>
2026-06-10 15:11:17 +0200

    feat: aggiunta altezza, peso e tipo tramite PokeAPI

--- js.js
+++ js.js
@@ -8,13 +8,15 @@ fetch("evolines.json")
 
 document.getElementById("form").addEventListener("submit", cerca);
 
-function cerca(event) {
+async function cerca(event) {
   event.preventDefault();
 
   const ricerca = document.getElementById("pokemon").value.toLowerCase();
 
   document.getElementById("nome").innerHTML = "";
   document.getElementById("sprite").innerHTML = "";
+  document.getElementById("info").innerHTML = "";
+  document.getElementById("shiny").innerHTML = "";
 
   const lineaTrovata = pk.find((linea) =>
     linea.some(
@@ -29,11 +31,42 @@ function cerca(event) {
     return;
   }
 
+  let pokemonCercato = lineaTrovata.find(
+    (pokemon) =>
+      pokemon.nome.toLowerCase() === ricerca || pokemon.id == ricerca,
+  );
+
+  let res = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${pokemonCercato.id}`,
+  );
+
+  let data = await res.json();
+
+  let altezza = data.height / 10;
+  let peso = data.weight / 10;
+  let tipi = data.types.map((t) => t.type.name).join(", ");
+
+  document.getElementById("info").innerHTML = `
+        <p>Altezza: ${altezza} m</p>
+        <p>Peso: ${peso} kg</p>
+        <p>Tipo: ${tipi}</p>
+    `;
+  document.getElementById("sprite").innerHTML = "<h2>Versione Normali</h2>";
   lineaTrovata.forEach((pokemon) => {
     document.getElementById("sprite").innerHTML += `
-            <div class="pokemon">
+            <div class="pokemon"><br>
                 <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png" alt="${pokemon.nome}">
                 <p>${pokemon.nome}</p>
+            </div>
+        `;
+  });
+
+  document.getElementById("shiny").innerHTML = "<h2>Versione Shiny ✨</h2>";
+  lineaTrovata.forEach((pokemon) => {
+    document.getElementById("shiny").innerHTML += `
+            <div class="pokemon">
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${pokemon.id}.png" alt="${pokemon.nome} shiny">
+                <p>${pokemon.nome} ✨</p>
             </div>
         `;
   });
